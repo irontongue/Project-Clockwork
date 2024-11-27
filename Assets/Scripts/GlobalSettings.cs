ca92db26e97ec42f319d05ba4d22b29b34c274b0
MrKnighty <dooty2nd@hotmail.com>
2024-11-27 16:56:35 +1030

    seperate music slider

--- Assets/Scripts/GlobalSettings.cs
+++ Assets/Scripts/GlobalSettings.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GlobalSettings : MonoBehaviour
 {
     static public float audioVolume = 0.5f;
+    static public float musicVolume = 0.5f;
     static public float mouseSensitivty = 1f;
 
  
--- Assets/SettingsSetter.cs
+++ Assets/SettingsSetter.cs
@@ -7,15 +7,16 @@ using UnityEngine.Scripting;
 public class SettingsSetter : MonoBehaviour
 {
 
-    [SerializeField] Slider audioSlider, mouseSlider;
+    [SerializeField] Slider audioSlider,musicVolumeSlider, mouseSlider;
     [SerializeField] AudioSource music;
-    
 
+    [SerializeField] float musicVolumeMultiplyer = 0.25f;
     private void Start()
     {
         audioSlider.value = GlobalSettings.audioVolume;
         mouseSlider.value = GlobalSettings.mouseSensitivty;
-        music.volume = audioSlider.value * 0.05f;
+        musicVolumeSlider.value = GlobalSettings.musicVolume ;
+        music.volume = musicVolumeSlider.value * musicVolumeMultiplyer;
         Application.targetFrameRate = 9999;
         QualitySettings.maxQueuedFrames = 2;
 
@@ -25,11 +26,16 @@ public class SettingsSetter : MonoBehaviour
     public void AudioSlider()
     {
         GlobalSettings.audioVolume = audioSlider.value;
-        music.volume = audioSlider.value * 0.05f;
+        
     }
     public void MouseSlider()
     {
         GlobalSettings.mouseSensitivty = mouseSlider.value;
+    }
+    public void MusicSlider()
+    {
+        GlobalSettings.musicVolume = musicVolumeSlider.value;
+        music.volume = musicVolumeSlider.value * musicVolumeMultiplyer;
     }
 
 }
